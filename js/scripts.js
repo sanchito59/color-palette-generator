9408827514dc1263704e2b99aacac7d9c868b53a
Chris Sanchez <c.sanch7@gmail.com>
2019-11-26 16:46:21 -0800

    initialize repo, set up a simple grid template

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function Construction() {
+    this.palette = palette;
+}
+
+Construction.prototype.method = function () {
+    
+}
+
+
+$(document).ready(function () {
+    $('#formId').submit(function (event) {
+        event.preventDefault();
+
+    })
+});
