a502d35c79c3828dfe9b5a883d99aafb9a3fde09
Chris Sanchez <c.sanch7@gmail.com>
2019-11-26 17:01:41 -0800

    fix template structure, add basic logic to generate 3 colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
-function Construction() {
-    this.palette = palette;
+class Construction {
+    constructor(color1, color2, color3) {
+        this.color1 = color1;
+        this.color2 = color2;
+        this.color3 = color3;
+        this.palette = palette;
+    }
+    generateColor(r,g,b) {
+        
+    }
 }
 
-Construction.prototype.method = function () {
-    
-}
+const r = Math.floor(Math.random() * 255); 
+const g = Math.floor(Math.random() * 255);
+const b = Math.floor(Math.random() * 255);
 
-
-$(document).ready(function () {
-    $('#formId').submit(function (event) {
-        event.preventDefault();
-
-    })
-});
+console.log(`rgb(${r},${g},${b})`);
