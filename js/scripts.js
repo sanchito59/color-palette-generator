c57bcca0a9293c3c82f25db05c6a57fe22113e65
Chris Sanchez <c.sanch7@gmail.com>
2019-11-27 00:02:45 -0800

    add paletteArray() function and setColors() function, setColors() has work in progress DOM text interaction, need to rename functions potentially

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    let palette = [];
+    let colorPalette = [];
     let colorLength = 5;
+    let squares = $('.color-box');
     function generateColor() {
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
@@ -9,40 +10,25 @@ $(document).ready(function () {
     }
     generateColor();
 
-    function paletteArray(){
-        for(let i = 0; i < colorLength; i++){
-            palette.push(generateColor());
+    function paletteArray() {
+        for (let i = 0; i < colorLength; i++) {
+            colorPalette.push(generateColor());
         }
-        console.log(palette);
+        console.log('inside paletteArray(): ', colorPalette);
+        return colorPalette;
     }
     paletteArray();
 
+    function setColors() {
+        for (let i = 0; i < colorPalette.length; i++) {
+            squares[i].style.background = colorPalette[i];
+            console.log('colorPalette: ', i, colorPalette[i]);
+            // squares.text(colorPalette[i]); //WIP
+        }
+    }
+    setColors();
+
     $('#new-palette-button').click(function () {
-        // const color1 = generateColor();
-        // const color2 = generateColor();
-        // const color3 = generateColor();
-        // const color4 = generateColor();
-        // const color5 = generateColor();
-        // document.getElementById('color1').style.background = color1;
-        // document.getElementById('color2').style.background = color2;
-        // document.getElementById('color3').style.background = color3;
-        // document.getElementById('color4').style.background = color4;
-        // document.getElementById('color5').style.background = color5;
-        // $('#color1').html(color1);
-        // $('#color2').html(color2);
-        // $('#color3').html(color3);
-        // $('#color4').html(color4);
-        // $('#color5').html(color5);
+
     });
 })
-
-// let colors = 5;
-// for (let i = 0; i < colors; i++) {
-//     const colors = generateColor();
-//     console.log(`${i}: ${colors}`);
-//     // console.log('i: ', colors[i]);
-//     document.getElementById('color1').style.background = colors;
-//     $('#color1').html(colors);
-
-
-// }
