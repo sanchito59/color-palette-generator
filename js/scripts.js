5566377b58bc4de105b8177d17921236d6073437
Chris Sanchez <c.sanch7@gmail.com>
2019-11-26 17:13:04 -0800

    generate string of RGB, variable definition bug

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,23 @@ class Construction {
         this.color3 = color3;
         this.palette = palette;
     }
-    generateColor(r,g,b) {
-        
+    generateColor(r, g, b) {
+
     }
 }
 
-const r = Math.floor(Math.random() * 255); 
-const g = Math.floor(Math.random() * 255);
-const b = Math.floor(Math.random() * 255);
+
+$(document).ready(function () {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    const newRGB = 'rgb' + '(' + r + ',' + g + ',' + b + ')';
+    console.log(newRGB);
+    $('#color1').click('on', function () {
+        alert('clicked color1');
+        $('#color1').css('background-color', 'newRGB');
+    });
+})
 
 console.log(`rgb(${r},${g},${b})`);
