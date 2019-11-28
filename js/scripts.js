7c40eb6898471ce8a332839b3cb0ffa83424dd11
Chris Sanchez <c.sanch7@gmail.com>
2019-11-27 17:20:36 -0800

    add full functionality for adding rgb info to each color div, new unique colors on page load and new unique colors on new-color-palette button

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     let colorPalette = [];
     let colorLength = 5;
     let squares = $('.color-box');
+
     function generateColor() {
         let r = Math.floor(Math.random() * 256);
         let g = Math.floor(Math.random() * 256);
@@ -12,14 +13,13 @@ $(document).ready(function () {
         for (let i = 0; i < colorLength; i++) {
             colorPalette.push(generateColor());
         }
-        console.log('inside paletteArray(): ', colorPalette);
         return colorPalette;
     }
     function setColors() {
         for (let i = 0; i < colorPalette.length; i++) {
             squares[i].style.background = colorPalette[i];
-            console.log('colorPalette: ', i, colorPalette[i]);
-            // squares.text(colorPalette[i]); //WIP
+
+            $(squares[i]).text(colorPalette[i]); //WIP
         }
     }
     function init() {
@@ -28,6 +28,7 @@ $(document).ready(function () {
         paletteArray();
         setColors();
     }
+    init();
     $('#new-palette-button').click(function () {
         init();
     });
