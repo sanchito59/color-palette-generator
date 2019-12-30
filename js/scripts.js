74090bd35c7eedf143c0f85a05b81dcff16af2ee
Chris Sanchez <c.sanch7@gmail.com>
2019-12-30 14:14:08 -0800

    update DOM, work in progress bug fix for displayPalettes, committing to master before branching

--- js/scripts.js
+++ js/scripts.js
@@ -56,40 +56,22 @@ class Palette {
     }
 }
 
-// function displayPalette(paletteToDisplay) {
-//     console.log('displayPalette: ',colorPalette)
-//     const paletteList = $("ul#palettes");
-//     const htmlForPaletteInfo = "";
-//     paletteToDisplay.palettes.forEach(function(palette) {
-//       htmlForPaletteInfo += "<li id=" + palette.id + ">" + palette.colorPalette[0] + " " + palette.colorPalette[1] + " " + palette.colorPalette[2] + " " + palette.colorPalette[3] + " " + palette.colorPalette[4] + " " +"</li>";
-//     });
-//     paletteList.html(htmlForPaletteInfo);
-//   };
-// User Interface Logic ---------
-// function attachpaletteListeners() {
-//     $("#buttons").on("click", ".deleteButton", function () {
-//         ColorPaletteHolder.deletePalette(this.id);
-//     });
-// };
-
 let colorPaletteHolder = new ColorPaletteHolder();
 // let palettes = new PaletteHolder();
 $(document).ready(function () {
     // attachpaletteListeners();
     function savePalette() {
         let newPalette = new Palette(colorPalette[0], colorPalette[1], colorPalette[2], colorPalette[3], colorPalette[4]);
-        console.log(colorPalette)
         colorPaletteHolder.addPalette(colorPalette);
         console.log('savedPalette(): ', colorPaletteHolder);
         return colorPaletteHolder;
     }
-    // let fullBook = Object.assign(newpalette, newAddress);
     //NEW CODE^^^^ needs work
 
     let colorPalette = [];
     const colorLength = 5;
     const squares = $('.color-box');
-    const savedPalette = $('.color-box2');
+    
 
     function generateColor() {
         let r = Math.floor(Math.random() * 256);
@@ -113,9 +95,13 @@ $(document).ready(function () {
     }
 
     function displayPalette(paletteToDisplay) {
-        console.log(savedPalette);
-        paletteToDisplay.palettes.forEach(function (palette) {
-            const paletteLayout = `<li id='${colorPaletteHolder.currentId}'><h4>Palette: </h4>
+        let paletteList = $('li #' + colorPaletteHolder.currentId);
+        console.log(paletteList);
+        // console.log('LOOKY HERE: ', paletteToDisplay.palettes);
+        $('#palettes').show();
+        for(let i = 0; i < paletteToDisplay.palettes.length; i++){
+            console.log('TEST', i);
+            const paletteLayout = `<li id='${colorPaletteHolder.currentId}'><h4>Palette: ${colorPaletteHolder.currentId}</h4>
             <div class="wrapper2">
             <div class="color-box2"></div>
             <div class="color-box2"></div>
@@ -124,17 +110,15 @@ $(document).ready(function () {
             <div class="color-box2"></div>
             </div></li>`
             //This code produces duplicates saving "1 then 2 then 3, not appending proper colors either"
-            console.log(colorPaletteHolder.currentId);
+            // console.log(colorPaletteHolder.currentId);
+            // console.log('HERE: ', savedPalette[i]);
             $('#palettes').append(paletteLayout);
-            for (let i = 0; i < colorPalette.length; i++) {
+            const savedPalette = $('.color-box2');
+            for (let i = 0; i < paletteLayout.length; i++) {
+                console.log('forLoop savedPal:', savedPalette[i]);
                 savedPalette[i].style.background = colorPalette[i];
             }
-            // $(savedPalette).text('');
-        })
-
-        // let paletteList = $('#' + colorPaletteHolder.currentId);
-        // console.log(paletteList);
-
+        }
     }
 
     function init() {
@@ -152,8 +136,8 @@ $(document).ready(function () {
     });
 
     $('#save-palette-button').click(function () {
+        console.log('colorPaletteHolder: ', colorPaletteHolder)
         savePalette();
-        // displayPalette();
         displayPalette(colorPaletteHolder);
     });
 })
